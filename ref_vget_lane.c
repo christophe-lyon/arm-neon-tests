c8c46ba33bffbbf7df54229c00ae3a45366fc72d
Christophe Lyon <christophe.lyon@st.com>
2013-03-29 18:29:55 +0100

    Remove useless code.

--- ref_vget_lane.c
+++ ref_vget_lane.c
@@ -36,7 +36,6 @@ void exec_vget_lane (void)
   /* vec=vget_lane(vec, lane), then store the result.  */
 #define TEST_VGET_LANE(Q, T1, T2, W, N, L)				\
   VAR(var, T1, W) = vget##Q##_lane_##T2##W(VECT_VAR(vector, T1, W, N), L); \
-  vst1##Q##_##T2##W(VECT_VAR(result, T1, W, N), VECT_VAR(vector, T1, W, N)); \
   fprintf(ref_file, "%s: %" PRIx##W "\n", "vget"STR(Q)"_lane_"STR(T2##W), VAR(var, T1, W))
 
   /* Special variant for floating-point */
@@ -47,7 +46,6 @@ void exec_vget_lane (void)
 
 #define TEST_VGET_LANE_F(Q, T1, T2, W, N, L)				\
   VAR(var, T1, W) = vget##Q##_lane_##T2##W(VECT_VAR(vector, T1, W, N), L); \
-  vst1##Q##_##T2##W(VECT_VAR(result, T1, W, N), VECT_VAR(vector, T1, W, N)); \
   var_int##W##_float##W.var_float##W = VAR(var, T1, W);		\
   fprintf(ref_file, "%s: %" PRIx##W "\n", "vget"STR(Q)"_lane_"STR(T2##W), var_int##W##_float##W.var_int##W)
 
